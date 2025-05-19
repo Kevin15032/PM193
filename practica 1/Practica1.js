ee9af43b9b1904fa16e81c3ba12d1b7a70685560
Kevin15032 <kdtorres1503@gmail.com>
2025-05-19 15:57:10 -0600

    .

--- practica 1/Practica1.js
+++ practica 1/Practica1.js
@@ -3,9 +3,12 @@ const edad = 25;
 
 nombre = "Kevin dominguez ";
 
-const saludo = "Hola " + nombre + " tienes " + edad + " años";
+const saludo = `Hola, ${nombre}. Tienes ${edad} años.`;
 console.log(saludo);
 
 
 const cuadrado = numero => numero * numero;
-console.log(cuadrado(9))
+console.log(cuadrado(9));
+
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+console.log(saludoPersonalizado("Kevin", 25));
