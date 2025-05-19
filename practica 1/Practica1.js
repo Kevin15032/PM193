ee482037d5b9c3bed6086a3195b69d9b797f4db9
Kevin15032 <kdtorres1503@gmail.com>
2025-05-19 15:44:41 -0600

    b

--- practica 1/Practica1.js
+++ practica 1/Practica1.js
@@ -5,3 +5,7 @@ nombre = "Kevin dominguez ";
 
 const saludo = "Hola " + nombre + " tienes " + edad + " años";
 console.log(saludo);
+
+
+const cuadrado = numero => numero * numero;
+console.log(cuadrado(9))
