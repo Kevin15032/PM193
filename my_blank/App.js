4e65efcf92bf8bde824ce4295cabfc74cec7345c
Kevin15032 <kdtorres1503@gmail.com>
2025-06-16 20:27:50 -0600

    actividad 6

--- my_blank/App.js
+++ my_blank/App.js
@@ -4,31 +4,31 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import react, {useState} from'react';
 
 
-const Texto =()=>{
+const Texto =({style})=>{
   const[contenido,setContenido]= useState ('Hola mundo');
   const actulizaTexto=() => {setContenido('Estado Modificado')}
   return (
-    <Text onPress={actulizaTexto}>{contenido}</Text>
+    <Text style={[styles.Text,style]} onPress={actulizaTexto}>{contenido}</Text>
   )
 }
 
-const Boton = () =>{
-  const[conteiner1,conteiner2]= useState('Boton');
-  const actulziarBoton = () => {conteiner2 ('Boton precionado')}
-  return(
-    <Button title={conteiner1} onPress={actulziarBoton}></Button>
-  )
-}
+// const Boton = () =>{
+//   const[conteiner1,conteiner2]= useState('Boton');
+//   const actulziarBoton = () => {conteiner2 ('Boton precionado')}
+//   return(
+//     <Button title={conteiner1} onPress={actulziarBoton}></Button>
+//   )
+// }
 // Zona 2: Main 
 export default function App() {
   return (
 
     <View style={styles.container}>
 
-      <Texto >Hola</Texto>
-      <Texto >Mundo</Texto>
-      <Texto >react native</Texto>
-      <Boton></Boton>
+      <Texto style={styles.azul}></Texto>
+      <Texto style={styles.verde}></Texto>
+      <Texto style={styles.negro}></Texto>
+      {/* <Boton></Boton> */}
       <StatusBar style="auto" />
 
     </View>
@@ -42,7 +42,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'strech',
     justifyContent: 'center',
+    
   },
+  Text:{
+    color:'white',
+    fontSize: 27
+  },
+  azul:{backgroundColor:'blue'},
+  verde:{backgroundColor:'green'},
+  negro:{backgroundColor:'black'}
+  
+  
 });
